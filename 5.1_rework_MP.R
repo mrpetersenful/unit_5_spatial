6db27bbea60b89ea16bc4a8155c00a94259e6c01
mrpetersenful <mpetersen@seoe.sc.edu>
2023-07-16 10:55:56 -0400

    Started 5.1

---
+++ 5.1_rework_MP.R
@@ -0,0 +1,5 @@
+### Lesson 5.1: Bathymetry and chlorophyll rasters
+## New packages: raster, mapdata, marmap
+## New functions: raster(), rasterToPoints(), geom_raster(), geom_tile(), map_data(), raster::crop(), raster::extent(), marmap::getNOAA.bathy(), geom_polygon(), coord_fixed(), marmap::fortify.bathy(), marmap::as.raster(), geom_contour(), raster::resample(), raster::stack()
+
+## Raster data
