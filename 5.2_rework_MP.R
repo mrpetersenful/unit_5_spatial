a256840383d00802f5f532377d1a05683c9b9f83
mrpetersenful <mpetersen@seoe.sc.edu>
2023-07-19 22:48:33 -0400

    Completed 5.2 - good information about types of file information that give you maps. Clearly I need to read more about them.

--- 5.2_rework_MP.R
+++ 5.2_rework_MP.R
@@ -22,13 +22,13 @@
 
 # Geometry types:
 
-# Point: a single point
-# Multipoint: multiple points
-# Linestring: sequence of two or more points connected by straight lines
-# Multilinestring: multiple lines
-# Polygon: a closed ring with zero or more interior holes
-# Multipolygon: multiple polygons
-# Geometrycollection: any combination of the above types
+# POINT: a single point
+# MULTIPOINT: multiple points
+# LINESTRING: sequence of two or more points connected by straight lines
+# MULTILINESTRING: multiple lines
+# POLYGON: a closed ring with zero or more interior holes
+# MULTIPOLYGON: multiple polygons
+# GEOMETRYCOLLECTION: any combination of the above types
 
 # All functions and methods in the sf package that operate on spatial data are preceded by the prefix st_ which refers to spatial type. 
 
@@ -48,22 +48,114 @@ library(marmap)
 
 ## Reading and organizing disparate spatial datasets
 
+# Now we are going to load in three right whale datasets. (This data is all publicly available, but it required me to combine multiple, messy, sources as well as data requests rom the North Atlantic Right Whale Consortium). We'll load:
+
+# 1. Shapefiles that contain polygons corresponding to the boundaries of right whale critical habitats in the US in 2017
+# 2. A .csv file that contains the lat/lon boundaries of right whale critical habitats in Canada in 2017
+# 3. A .csv file that contains the location and other info about right whale carcasses found in 2017
+
+# To read in the US critical habitat shapefiles, we use sf::st_read(). We take a look at the data and see that the CRS is EPSG code 4269, which is the NAD83 projection. [The 4-digit EPSG (European Petroleum Survey Group Geodetic Parameter Dataset) is a public registry of spatial reference systems. The NAD83 projection is the North American Datum 1983 CRS, which is most commonly used by US federal agencies.] In this exercise, we are going to use CRS WGS84 [World Geodetic System 1984; EPSG 4326] so we'll use st_transform() to transform our data from one CRS to another. 
+
+# Carcass location data
+carcass = read.csv('data/RW_carcasses_2017.csv')
+
+# Read in US critical habitat shapefiles
+# https://www.greateratlantic.fisheries.noaa.gov/educational_resources/gis/data/index.html
+
+USA_crit_hab = st_read('data/North_Atlantic_Right_Whale_Critical_Habitat/', 'North_Atlantic_Right_Whale_Critical_Habitat')
+USA_crit_hab
+USA_crit_hab_sf = st_transform(USA_crit_hab, crs=4326)
+# CRS = "+proj=longlat +datum=WGS84"
+
+# To get the Canadian critical habitat boundaries, we read in the series of polygon points provided in a simple .csv file. These are latitude/longitude coordinates with the CRS WGS84. We can convert the data frame into a simple features data frame using st_as_sf(). To do this, we must tell the function st_as_sf() which columns should be used as the X and Y coordinates, and what the CRS is. This gives us a simple features data frame with point geometries. 
+
+# Next we need to turn our points into polygons (i.e. the boundaries of our critical habitats). To do this we can use dplyr functions that have been created for simple features. We'll group our POINTS by habitat and country (grouping by country doesn't accomplish anything in this case since all of the points are in Canada, but including it in the grouping allows us to retain this column when we use summarize()). Then we'll use summarize to collapse the POINTS into 2 MULTIPOINT geometry types (one for Grand Manan Basin, one for Roseway Basin). As you recall from using summarize() previously, this function drops all columns except the column(s) that the data have been grouped by and the column(s) that you create in the summarize() function. However, when using dplyr on sf objects, the geometries are "sticky", meaning that the geometry column of sf objects is retained unless the user deliberately removes it. So we have used summarize() to convert the POINTs in each habitat to a single MULTIPOINT geometry. Whew. Finally, we use the parameter do_union=FALSE. This triggers the summarize() function to use st_combine() instead of st_union() to collapse our POINTs into MULTIPOINTs. This only matters because the st_union() function does not preserve POINT order, and if our POINTs get out of order, then the POLYGON that we create will be wonky (check out ?summarise.sf). Finally, we use st_cast() to turn our MULTIPOINT geometries into POLYGONs. 
+
+# Load in Canadian RW critical habitat coordinates
+# http://www.dfo-mpo.gc.ca/species-especes/profiles-frofils/rightwhaleNA-baleinenoireAN-eng.html
+
+CAN_crit_hab = read.csv('data/NARW_canadian_critical_habitat_2017.csv')
+head(CAN_crit_hab)
+
+# Turn data frame into sf points, then sf polygon
+CAN_crit_hab_sf = CAN_crit_hab %>%
+  st_as_sf(coords=c("lon","lat"), crs=4326) %>%
+  dplyr::group_by(habitat, country) %>%
+  dplyr::summarise(do_union=FALSE) %>% 
+  st_cast("POLYGON")
+print(CAN_crit_hab_sf)
+
+# Now let's get our USA habitat and our Canada habitat data ready to be joined. We'll give the USA data the same attributes as the Canadian data: habitat and country. Then the USA and Canadian habitat data have the same attributes, the same geometric types (POLYGON) and the same CRS. Now we can just use rbind() to combine them into a single sf object!
+
+# Simply USA_crit_hab data frame to match CAN_crit_hab
+plot(USA_crit_hab_sf$geometry[1], axes=TRUE)
+plot(USA_crit_hab_sf$geometry[2], axes=TRUE)
+
+USA_crit_hab_sf$habitat=c("NMFS_GOM", "SEUS")
+USA_crit_hab_sf$country="USA"
+USA_crit_hab_sf = USA_crit_hab_sf %>% dplyr::select(country, habitat, geometry) # drops all other variables from shapefile
+head(USA_crit_hab_sf)
+
+
+# Join the USA and Canada critical habitat sf objects
+crit_hab = rbind(USA_crit_hab_sf, CAN_crit_hab_sf)
 
 
 
+## Making maps
+
+# So that was a lot of hard work merging our USA and Canada datasets because their original formats were very different. Different file types (.csv vs. shapefile), different geometries (coordinates vs. polygons), different attributes and different CRSs. Now that we have combined these disparate datasets, we can use ggplot2 tools to turn this spatial vector data into maps!
+
+# Set GOM + GSL map limits
+lon_bounds = c(-72, -54)
+lat_bounds = c(39, 53)
+
+# Coastline data
+world_map = map_data("worldHires", ylim=lat_bounds, xlim=lon_bounds)
+
+# Plot critical habitats and carcass locations 
+dev.new()
+ggplot() + 
+  geom_polygon(data = world_map, aes(x = long, y = lat, group = group), fill = "black") + 
+  geom_sf(data = crit_hab, alpha = 0.5, aes(fill=country)) + 
+  geom_point(data = carcass, aes(x = Longitude, y = Latitude, color = Carcass_position), size=2) + 
+  coord_sf(1.3, xlim = lon_bounds, ylim = lat_bounds) + 
+  ylab("Latitude") + xlab("Longitude") + theme_classic()
 
 
+# We can use what we learned in the previous tutorial to add bathymetry data beneath our critical habitat polygons and our carcass data. 
+
+install.packages("ggnewscale")
+library(ggnewscale)
+
+bath_m_raw = marmap::getNOAA.bathy(lon1 = lon_bounds[1], lon2 = lon_bounds[2],
+                                   lat1 = lat_bounds[1], lat2 = lat_bounds[2], resolution = 4)
+
+# Convert bathymetry to data frame
+bath_m_fortify = marmap::fortify.bathy(bath_m_raw)
+bath_m = bath_m_fortify %>%
+  mutate(depth_m = ifelse(z>0, NA, z)) %>%
+  dplyr::select(-z)
+head(bath_m)
+summary(bath_m)
+
+# Plot critical habitats and carcass locations over bathymetry
+rw_map = ggplot() + 
+  geom_raster(data = bath_m, aes(x = x, y = y, fill = depth_m), alpha=0.75) + 
+  scale_fill_gradientn(colors=c("black", "navy", "blue4", "lightblue"), 
+                       values = scales::rescale(c(-5000, -3000, -300, 0)),
+                       name="Depth (m)") + 
+  geom_polygon(data = world_map, aes(x = long, y = lat, group = group), fill = "black", color = NA) +
+  geom_sf(data=crit_hab, alpha = 0.5, fill='yellow') + 
+  geom_point(data = carcass, aes(x = Longitude, y = Latitude, color = Carcass_position), size=2) + 
+  coord_sf(1.3, xlim = lon_bounds, ylim = lat_bounds) +
+  ylab("Latitude") + xlab("Longitude") + theme_classic()
+
+dev.new()
+rw_map
 
 
-
-
-
-
-
-
-
-
-
+ggsave(rw_map, filename='figures/RW_habitats.pdf', device="pdf", height=5, width=7)
 
 
 
